5f28a7de82fc0e7312e4b9e39faa0928390cacf0
malik583 <malik.shaik@costrategix.com>
2020-02-12 11:16:16 +0530

    Rearranged imports

--- src/main/java/com/costrategix/auth/controller/UserController.java
+++ src/main/java/com/costrategix/auth/controller/UserController.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.swing.text.html.HTML;
-
 @RestController
 public class UserController {
 
--- src/main/java/com/costrategix/auth/service/UserService.java
+++ src/main/java/com/costrategix/auth/service/UserService.java
@@ -3,11 +3,11 @@ package com.costrategix.auth.service;
 import com.costrategix.auth.model.User;
 import com.costrategix.auth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.ArrayList;
 
